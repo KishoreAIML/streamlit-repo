141befd3ba18a72933cf94f769196a040fff48bf
Kishore T <167704553+KishoreAIML@users.noreply.github.com>
2025-09-26 14:04:06 +0530

    Update menu.py

--- menu.py
+++ menu.py
@@ -7,3 +7,13 @@ st.header("This is a header")
 st.subheader("This is a subheader")
 st.text("Hello GeeksForGeeks!!!")
 st.markdown("This is markdown")
+st.success("Success")
+
+st.info("Information")
+
+st.warning("Warning")
+
+st.error("Error")
+
+exp = ZeroDivisionError("Trying to divide by Zero")
+st.exception(exp)
