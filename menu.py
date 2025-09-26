17a3d3a811ea98cd677d58919e6682fa2f29654a
Kishore T <167704553+KishoreAIML@users.noreply.github.com>
2025-09-26 14:07:13 +0530

    Update menu.py

--- menu.py
+++ menu.py
@@ -1,5 +1,6 @@
 
 import streamlit as st
+from PIL import Image
 
 st.title("Hellow World!")
 st.header("This is a header") 
@@ -17,3 +18,6 @@ st.error("Error")
 
 exp = ZeroDivisionError("Trying to divide by Zero")
 st.exception(exp)
+
+image = Image.open("https://www.google.com/imgres?q=llms%20interview%20questions&imgurl=https%3A%2F%2Fcdn.analyticsvidhya.com%2Fwp-content%2Fuploads%2F2024%2F10%2FLLMs.webp&imgrefurl=https%3A%2F%2Fwww.analyticsvidhya.com%2Fblog%2F2024%2F04%2Fllm-interview-questions%2F&docid=oXlEYVFgTEH5OM&tbnid=wzCkvLh6fYyjeM&vet=12ahUKEwiZjcTtgvaPAxXiS2wGHdj-I60QM3oECBcQAA..i&w=872&h=490&hcb=2&ved=2ahUKEwiZjcTtgvaPAxXiS2wGHdj-I60QM3oECBcQAA")
+st.image(image , width = 200)
